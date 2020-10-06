0011e34fb1176439cd59bd3fd71efa7719929805
dhvanibhatt3 <dbhatt7@horizon.csueastbay.edu>
2020-10-05 23:23:06 -0700

    Updates node modules and index.js

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const pool = new Pool({
 })
 
 /* GET home page. */
-    .get('/db', async (req, res) => {
+    router.get('/db', async (req, res) => {
       try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM test_table');
